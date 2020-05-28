c4ca57e1e569ceac02f7d72682df79dd5ab938b9
myosubug <seanoh.dev@gmail.com>
2020-05-27 21:50:42 -0600

    scrapped up to job titles

---
+++ extract.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = "https://www.indeed.ca/jobs?as_and=software+engineer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&as_src=&salary=&radius=25&l=Canada&fromage=15&limit={LIMIT}&sort=&psf=advsrch&from=advancedsearch"
+
+
+def extract_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pagination = soup.find("div", {"class": "pagination"})
+    anchors = pagination.find_all('a')
+    pages = []
+    for anchor in anchors[:-1]:
+        pages.append(int(anchor.string))
+    return pages[-1]
+
+
+def extract_jobs(lastPage):
+    jobs = []
+    for page in range(lastPage):
+        result = requests.get(URL+"&start={0*LIMIT}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            title = result.find("h2", {"class": "title"}).find('a')["title"]
+            print(title)
+
+    return jobs
